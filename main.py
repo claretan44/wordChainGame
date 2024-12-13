e3a516a35313ac87c875076b59129c33e36e6359
cfsyj <cfsyjuco@gmail.com>
2024-12-13 23:45:56 +0800

    added basic ability to ask user for a word starting with a letter and getting the input back

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_a_letter():
+    # Use a breakpoint in the code line below to debug your script.
+    random_letter: str = "a"
+    print(f"Type a word starting with the letter {random_letter}")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_a_letter()
+    user_input: str = input ("Your word: ")
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
