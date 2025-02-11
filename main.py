44c616b9acd214cd67ebdec152a598fd55d1f3fa
cfsyj <cfsyjuco@gmail.com>
2025-02-11 22:47:55 +0800

    Added a file with list of animals Added the wonderwords package for generating words Created function to load list of words from a file

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
-# This is a sample Python script.
+from wonderwords import RandomWord
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def load_word_list(filename: str):
+    # Returns a list from a file with a series of words
+    words = []
+    with open(filename, "r", encoding = "UTF-8") as file:
+        for line in file:
+            words.append(line.strip())
+    return words
 
+def generate_word(letter: str):
+    # Returns a random word that begins with the letter passed in
+    pass
 
 def print_a_letter():
-    # Use a breakpoint in the code line below to debug your script.
-    random_letter: str = "a"
-    print(f"Type a word starting with the letter {random_letter}")  # Press Ctrl+F8 to toggle the breakpoint.
+    # Generates the first starting letter
+    first_letter: str = random.choice("abcdefghijklmnopqrstuvwxyz")
+    print(f"Type a word starting with the letter {first_letter}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    # Load list of animals to memory
+    animal_list = load_word_list("animals.txt")
+
+    # Create a word generator from the list
+    generator = RandomWord(animal = animal_list)
+
+    # Pick a random first letter
     print_a_letter()
-    user_input: str = input ("Your word: ")
+
+    # TODO: Get user input (we should pick if sentence case or lowercase)
+    # user_input: str = input ("Your word: ")
+
+    print(generator.word())
+    #animal list generated from goodgoodgood.co
+
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
